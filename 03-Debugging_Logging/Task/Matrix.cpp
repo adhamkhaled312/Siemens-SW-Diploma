41614048b8f9ee7bdeb5748f7252c7600bbc7d2f
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2024-07-24 20:39:44 +0300

    Solve assignment + add materials

--- 03-Debugging_Logging/Task/Matrix.cpp
+++ 03-Debugging_Logging/Task/Matrix.cpp
@@ -40,7 +40,7 @@ public:
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < other.cols; ++j) {
                 for (int k = 0; k < cols; ++k) {
-                    result.data[i][j] = data[i][k] * other.data[k][j];
+                    result.data[i][j] += data[i][k] * other.data[k][j];
                 }
             }
         }
