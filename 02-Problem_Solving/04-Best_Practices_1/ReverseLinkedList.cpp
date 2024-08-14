9ac79a5f7e064ac073e6c05dd111d219ce6d3333
adhamkhaled312 <adhamkhaled312@gmail.com>
2024-08-14 18:35:44 +0300

    add best practices problems solutions

---
+++ 02-Problem_Solving/04-Best_Practices_1/ReverseLinkedList.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/reverse-linked-list/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* prev=head;
+        ListNode* curr=head->next;
+        ListNode* next=head->next->next;
+        while(next!=NULL){
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            next=next->next;
+
+        }
+        curr->next=prev;
+        head->next=NULL;
+        return curr;
+    }
+};
